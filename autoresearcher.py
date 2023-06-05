e831d004b221ec0b66cad5fa42e14019c103fb9d
IqraShahid-dev <iqra.qurbaniapp@gmail.com>
2023-06-05 11:16:40 +0500

    mini getPapers project

---
+++ autoresearcher.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+import os
+import sys
+import json
+from termcolor import colored
+from dotenv import load_dotenv
+import openai
+
+from symantic_scholar_loader import SemanticScholarLoader
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+assert OPENAI_API_KEY, "OPENAI_API_KEY environment variable is missing from .env"
+
+# Configure OpenAI
+openai.api_key = OPENAI_API_KEY
+
+
+def openai_call(
+    prompt: str, use_gpt4: bool = False, temperature: float = 0.5, max_tokens: int = 100
+):
+    """
+    Calls OpenAI API to generate a response to a given prompt.
+    Args:
+      prompt (str): The prompt to generate a response to.
+      use_gpt4 (bool, optional): Whether to use GPT-4 or GPT-3.5. Defaults to False.
+      temperature (float, optional): The temperature of the response. Defaults to 0.5.
+      max_tokens (int, optional): The maximum number of tokens to generate. Defaults to 100.
+    Returns:
+      str: The generated response.
+    Examples:
+      >>> openai_call("Hello, how are you?")
+      "I'm doing great, thanks for asking!"
+    Notes:
+      The OpenAI API key must be set in the environment variable OPENAI_API_KEY.
+    """
+    if not use_gpt4:
+        # Call GPT-3.5 turbo model
+        messages = [{"role": "user", "content": prompt}]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
+        return response.choices[0].message.content.strip()
+    else:
+        # Call GPT-4 chat model
+        messages = [{"role": "user", "content": prompt}]
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            n=1,
+            stop=None,
+        )
+        return response.choices[0].message.content.strip()
+
+
+# Generate keyword combinations for a given research question
+def generate_keyword_combinations(research_question):
+    """
+    Generates keyword combinations for a given research question.
+    Args:
+      research_question (str): The research question to generate keyword combinations for.
+    Returns:
+      list: A list of keyword combinations for the given research question.
+    Examples:
+      >>> generate_keyword_combinations("What is the impact of AI on healthcare?")
+      ["AI healthcare", "impact AI healthcare", "AI healthcare impact"]
+    """
+    keyword_combination_prompt = """
+    `reset`
+    `no quotes`
+    `no explanations`
+    `no prompt`
+    `no self-reference`
+    `no apologies`
+    `no filler`
+    `just answer`
+
+    Generate several keyword combinations based on the following research question: {research_question}.
+    Don't generate more than 5 keyword combinations.
+
+    The output should be structured like this:
+    Write "KeywordCombination:" and then list the keywords like so "Keyword,Keyword,Keyword"
+
+    """
+    prompt = keyword_combination_prompt.format(research_question=research_question)
+    response = openai_call(prompt, use_gpt4=False, temperature=0, max_tokens=200)
+    combinations = response.split("\n")
+    return [
+        combination.split(": ")[1]
+        for combination in combinations
+        if ": " in combination
+    ]
+
+
+def literature_review(research_question, SS_key=None):
+    print("SS_key :" + SS_key)
+    """
+    Generates an academic literature review for a given research question.
+    Args:
+      research_question (str): The research question to generate a literature review for.
+      SS_key (str, optional): The Semantic Scholar API key.
+    Returns:
+      str: The generated literature review.
+    Examples:
+      >>> literature_review('What is the impact of AI on healthcare?')
+      Research question: What is the impact of AI on healthcare?
+      Auto Researcher initiated!
+      Generating keyword combinations...
+      Keyword combinations generated!
+      Fetching top 20 papers...
+      Top 20 papers fetched!
+      Extracting research findings from papers...
+      Research findings extracted!
+      Synthesizing answers...
+      Literature review generated!
+      Academic Literature Review: ...
+      References:
+      1. ...
+      Keyword combinations used to search for papers: 1. AI healthcare, 2. impact AI healthcare
+    """
+    SemanticScholar = SemanticScholarLoader(SS_key)
+
+    print(
+        colored(
+            f"Research question: {research_question}", "yellow", attrs=["bold", "blink"]
+        )
+    )
+    print(colored("Auto Researcher initiated!", "yellow"))
+
+    # Generate keyword combinations
+    print(colored("Generating keyword combinations...", "yellow"))
+    keyword_combinations = generate_keyword_combinations(research_question)
+    print(colored("Keyword combinations generated!", "green"))
+    print("Keyword combinations starts:")
+    print(", ".join(keyword_combinations))
+    print("Keyword combinations ends:")
+
+    # Fetch the top 20 papers for the research question
+    search_query = research_question
+    print(colored("Fetching top 20 papers....", "yellow"))
+    top_papers = SemanticScholar.fetch_and_sort_papers(
+        search_query, keyword_combinations=keyword_combinations, year_range="2000-2023"
+    )
+    print(colored("Top 20 papers fetched!", "green"))
+    #print(json.dumps(top_papers))
+    return top_papers
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        research_question = sys.argv[1]
+        SS_key = sys.argv[2]
+    elif len(sys.argv) > 1:
+        research_question = sys.argv[1]
+        SS_key = None
+    else:
+        raise ValueError(
+            "No research question provided. Usage: python literature_review.py 'My research question'"
+        )
+
+    papers = literature_review(research_question, SS_key)
+    print("Papers starts:")
+    print(json.dumps(papers))
+    print("Papers ends:")
---
+++ symantic_scholar_loader.py
@@ -0,0 +1,101 @@
+import jellyfish
+import time
+
+from base_web_loader import BaseWebAPIDataLoader
+
+
+class SemanticScholarLoader(BaseWebAPIDataLoader):
+    SS_key = None
+
+    def __init__(self, SS_key):
+        """
+        Initializes the SemanticScholarLoader class.
+        Args:
+          SS_key (str, optional): Semantic Scholar partner key
+        Returns:
+          None
+        Notes:
+          Calls the superclass constructor with the SemanticScholar API URL.
+        """
+        super().__init__("https://api.semanticscholar.org/graph/v1/paper/search")
+        self.SS_key = SS_key
+
+    def fetch_data(self, search_query, limit=5, year_range=None):
+        """
+        Fetches data from the SemanticScholar API.
+        Args:
+          search_query (str): The query to search for.
+          limit (int, optional): The maximum number of results to return. Defaults to 100.
+          year_range (tuple, optional): A tuple of two integers representing the start and end year of the search. Defaults to None.
+          x-api-key (str, optional): Semantic scholar partner key
+        Returns:
+          list: A list of paper objects.
+        Examples:
+          >>> fetch_data("machine learning", limit=50, year_range=(2010, 2020))
+          [{...}, {...}, ...]
+        """
+        headers = {
+            "x-api-key": self.SS_key
+        }
+
+        params = {
+            "query": search_query,
+            "limit": limit,
+            "fields": "title,url,abstract,authors,citationStyles,journal,citationCount,year,externalIds"
+        }
+
+        if year_range is not None:
+            params["year"] = year_range
+
+        data = self.make_request("", params=params, headers=headers)
+        return data.get("data", [])
+
+    def fetch_and_sort_papers(
+            self,
+            search_query,
+            limit=5,
+            top_n=20,
+            year_range=None,
+            keyword_combinations=None,
+            weight_similarity=0.5,
+    ):
+        """
+        Fetches and sorts papers from the SemanticScholar API.
+        Args:
+          search_query (str): The query to search for.
+          limit (int, optional): The maximum number of results to return. Defaults to 100.
+          top_n (int, optional): The maximum number of results to return after sorting. Defaults to 20.
+          year_range (tuple, optional): A tuple of two integers representing the start and end year of the search. Defaults to None.
+          keyword_combinations (list, optional): A list of keyword combinations to search for. Defaults to None.
+          weight_similarity (float, optional): The weight to give to the similarity score when sorting. Defaults to 0.5.
+        Returns:
+          list: A list of the top `top_n` paper objects sorted by combined score.
+        Examples:
+          >>> fetch_and_sort_papers("machine learning", limit=50, top_n=10, year_range=(2010, 2020))
+          [{...}, {...}, ...]
+        """
+        papers = []
+        if keyword_combinations is None:
+            keyword_combinations = [search_query]
+
+        for combination in keyword_combinations:
+            papers.extend(self.fetch_data(combination, limit, year_range))
+            time.sleep(2)
+
+        max_citations = max(papers, key=lambda x: x["citationCount"])["citationCount"]
+
+        for paper in papers:
+            similarity = jellyfish.jaro_similarity(search_query, paper["title"])
+            normalized_citation_count = paper["citationCount"] / max_citations
+            paper["combined_score"] = (weight_similarity * similarity) + (
+                    (1 - weight_similarity) * normalized_citation_count
+            )
+
+        sorted_papers = sorted(papers, key=lambda x: x["combined_score"], reverse=True)
+
+        # deduplicate paper entries prior to taking top n results
+        sorted_dedup_papers = list(
+            {each_paper["paperId"]: each_paper for each_paper in sorted_papers}.values()
+        )
+
+        return sorted_dedup_papers[:top_n]
